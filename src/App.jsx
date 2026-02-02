a1fc4142b4197cb85ad2d995d9cb53c63200cb0f
abrarasif11 <faasif929@gmail.com>
2026-02-02 19:10:51 +0600

    avoid features card

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => (
     <Navbar />
     <Hero />
     <ShowcaseSection />
-    <FeatureCards />
+    {/* <FeatureCards /> */}
     <Experience />
     <TechSkill />
     <Testimonials />
